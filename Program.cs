a0ebf95e6d7fc6c852ecc08f1325bf976dd1fc04
Pihta <84587589+Zefirnutiy@users.noreply.github.com>
2024-02-24 23:32:07 +0500

    Add fifth solution

--- Program.cs
+++ Program.cs
@@ -1,6 +1,97 @@
-﻿// issue 1
+﻿using System;
+using System.Collections.Generic;
 
-static string Reverse(string s)
+class Issue
+{
+
+    static void Main()
+    {
+        Console.WriteLine("Input number of sort's method:" +
+            "\na - Quick sort" +
+            "\nb - Tree sort");        
+        string? sortMethod = Console.ReadLine();
+
+        if (sortMethod != "a" && sortMethod != "b")
+        {
+            Console.WriteLine("invalid method input");
+        }
+        else
+        {
+            Console.Write("input some string: ");
+            string? userInput = Console.ReadLine();
+            if (userInput == null)
+            {
+                Console.WriteLine("string is null");
+            }
+            else
+            {
+                Console.WriteLine(Issue1(userInput, sortMethod));
+            }
+        }
+    }
+
+
+    // issue 1
+    static string Issue1(string s, string sortMethod )
+    {
+        Console.WriteLine(sortMethod);
+     
+        if (s != ValidateString(s))
+        {
+            return ValidateString(s);
+        }
+
+        string operatedString = "";
+        string sortedString = "";
+
+        if (s.Length % 2 == 0)
+        {
+            int halfLengthOfString = s.Length / 2;
+
+            operatedString = Reverse(s.Substring(0, halfLengthOfString)) +
+            Reverse(s.Substring(halfLengthOfString, halfLengthOfString));
+        }
+        else
+        {
+            operatedString = Reverse(s) + s;
+        }
+
+        string quantityRepeatCharacters = CountRepeatCharacter(operatedString);
+        string largestSubString = FindLargestSubString(operatedString);
+        
+        if(sortMethod == "a")
+        {
+            sortedString = QuickSortString(operatedString);
+        }
+        else
+        {
+
+            BinaryTree tree = new BinaryTree();
+
+            foreach (char ch in operatedString)
+            {
+                tree.Insert($"{ch}");
+            }
+
+            string[] sortedArray = tree.InOrder();
+
+            foreach (string ch in sortedArray)
+            {
+                sortedString += ch;
+            }
+        }
+
+        string result = $"{operatedString}\n" +
+                "Repeat Characters:\n" +
+                $"{quantityRepeatCharacters}\n" +
+                "Largest valid Substring:\n" +
+                $"{largestSubString}\n\n" +
+                "Sorted string:\n" +
+                $"{sortedString}";
+
+        return result;
+    }
+    static string Reverse(string s)
 {
     string res = "";
 
@@ -11,55 +102,10 @@ static string Reverse(string s)
 
     return res;
 }
+  
 
-static string Issue1(string s)
-{
-    string result = "";
-    string quantityRepeatCharacters = "";
-    string operatedString = "";
-    string largestSubString = "";
-
-    if (s != ValidateString(s))
-    {
-        return ValidateString(s);
-    }
-
-    if (s.Length % 2 == 0)
-    {
-        int halfLengthOfString = s.Length / 2;
-
-        operatedString = Reverse(s.Substring(0, halfLengthOfString)) +
-        Reverse(s.Substring(halfLengthOfString, halfLengthOfString));
-
-        largestSubString = FindLargestSubString(operatedString);
-
-        quantityRepeatCharacters = CountRepeatCharacter(operatedString);
-
-        result = $"{operatedString}\n" +
-            $"Repeat Characters:\n" +
-            $"{quantityRepeatCharacters}\n" +
-            $"Largest valid Substring:\n" +
-            $"{largestSubString}";
-
-
-        return result;
-    }
-
-    operatedString = Reverse(s) + s;
-    quantityRepeatCharacters = CountRepeatCharacter(operatedString);
-    largestSubString = FindLargestSubString(operatedString);
-
-    result = $"{operatedString}\n" +
-            $"Repeat Characters:\n" +
-            $"{quantityRepeatCharacters}\n" +
-            $"Largest valid Substring:\n" +
-            $"{largestSubString}";
-
-    return result;
-}
-
-//issue 2
-static string ValidateString(string s)
+    //issue 2
+    static string ValidateString(string s)
 {
 
     string invalidUppercaseCharacters = "";
@@ -100,8 +146,9 @@ static string ValidateString(string s)
     return result;
 }
 
-// issue 3
-static string CountRepeatCharacter(string s)
+
+    // issue 3
+    static string CountRepeatCharacter(string s)
 {
     string result = "";
     foreach (var letter in s.Distinct().ToArray())
@@ -112,8 +159,9 @@ static string CountRepeatCharacter(string s)
     return result;
 }
 
-// issue 4
-static string FindLargestSubString(string s)
+
+    // issue 4
+    static string FindLargestSubString(string s)
 {
 
     string result = "no correct substring";
@@ -138,8 +186,7 @@ static string FindLargestSubString(string s)
 
     return result;
 }
-
-static bool validateSubString(string s)
+    static bool validateSubString(string s)
 {
     string validateCharacters = "aeiouy";
     bool startStringIsCorrect = false;
@@ -163,19 +210,213 @@ static bool validateSubString(string s)
 }
 
 
+    //issue 5
 
+    // quick sort
+    static string QuickSortString(string s)
+    {
 
+        int[] indexesOfLetter = stringToIntegerIndex(s);
 
+        quickSort(indexesOfLetter, 0, indexesOfLetter.Length - 1);
 
-Console.Write("Please input a string: ");
-string? userInput = Console.ReadLine();
+        string result = integerIndexToString(indexesOfLetter);
+        return result;
+    }   
 
-if (userInput != null)
+    static void quickSort(int[] arr, int left, int right)
 {
-    Console.WriteLine(Issue1(userInput));
-}else
+    if (left < right)
+    {
+        int pivot = partition(arr, left, right);
+
+        quickSort(arr, left, pivot - 1);
+        quickSort(arr, pivot + 1, right);
+    }
+}
+    static int partition(int[] arr, int left, int right)
 {
-    Console.WriteLine("you did not enter a string");
+    int pivot = arr[right];
+    int i = left - 1;
+
+    for (int j = left; j < right; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    int temp1 = arr[i + 1];
+    arr[i + 1] = arr[right];
+    arr[right] = temp1;
+
+    return i + 1;
 }
 
-Console.ReadKey(); 
+
+    //crutches
+    static int[] stringToIntegerIndex(string s)
+    {
+        var letters = new Dictionary<char, int>()
+    {
+        {'a', 0},
+        {'b', 1},
+        {'c', 2},
+        {'d', 3},
+        {'e', 4},
+        {'f', 5},
+        {'g', 6},
+        {'h', 7},
+        {'i', 8},
+        {'j', 9},
+        {'k', 10},
+        {'l', 11},
+        {'m', 12},
+        {'n', 13},
+        {'o', 14},
+        {'p', 15},
+        {'q', 16},
+        {'r', 17},
+        {'s', 18},
+        {'t', 19},
+        {'u', 20},
+        {'v', 21},
+        {'w', 22},
+        {'x', 23},
+        {'y', 24},
+        {'z', 25},
+    };
+
+        int[] indexesOfLetters = new int[s.Length];
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            indexesOfLetters[i] = letters[s[i]];
+        }
+
+        return indexesOfLetters;
+    }
+
+    static string integerIndexToString(int[] indexesOfLetter)
+    {
+        var reverseLetters = new Dictionary<int, char>()
+    {
+        {0, 'a'},
+        {1, 'b'},
+        {2, 'c'},
+        {3, 'd'},
+        {4, 'e'},
+        {5, 'f'},
+        {6, 'g'},
+        {7, 'h'},
+        {8, 'i'},
+        {9, 'j'},
+        {10, 'k'},
+        {11, 'l'},
+        {12, 'm'},
+        {13, 'n'},
+        {14, 'o'},
+        {15, 'p'},
+        {16, 'q'},
+        {17, 'r'},
+        {18, 's'},
+        {19, 't'},
+        {20, 'u'},
+        {21, 'v'},
+        {22, 'w'},
+        {23, 'x'},
+        {24, 'y'},
+        {25, 'z'},
+    };
+
+        string result = "";
+
+        foreach (var ch in indexesOfLetter)
+        {
+            result += reverseLetters[ch];
+        }
+
+        return result;
+    }
+
+}
+
+class Node
+{
+    public string Value;
+    public Node Left;
+    public Node Right;
+    public int Count;
+
+    public Node(string value)
+    {
+        Value = value;
+        Left = null;
+        Right = null;
+        Count = 1;
+    }
+}
+
+class BinaryTree
+{
+    private Node root;
+    private List<string> sortedList;
+
+    public BinaryTree()
+    {
+        root = null;
+        sortedList = new List<string>();
+    }
+
+    public void Insert(string value)
+    {
+        root = InsertRec(root, value);
+    }
+
+    private Node InsertRec(Node root, string value)
+    {
+        if (root == null)
+        {
+            root = new Node(value);
+            return root;
+        }
+
+        if (string.Compare(value, root.Value) < 0)
+        {
+            root.Left = InsertRec(root.Left, value);
+        }
+        else if (string.Compare(value, root.Value) > 0)
+        {
+            root.Right = InsertRec(root.Right, value);
+        }
+        else
+        {
+            root.Count++;
+        }
+
+        return root;
+    }
+
+    public string[] InOrder()
+    {
+        InOrderRec(root);
+        return sortedList.ToArray();
+    }
+
+    private void InOrderRec(Node root)
+    {
+        if (root != null)
+        {
+            InOrderRec(root.Left);
+            for (int i = 0; i < root.Count; i++)
+            {
+                sortedList.Add(root.Value);
+            }
+            InOrderRec(root.Right);
+        }
+    }
+}
